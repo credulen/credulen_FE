6ba3e0748840328e4f0273568d50ff7c5a6c49a8
pmanonline <petersonzoconis@gmail.com>
2024-10-21 13:52:32 +0100

    updated

--- src/pages/testing.jsx
+++ src/pages/testing.jsx
@@ -1,210 +1,105 @@
-import React, { useState, useEffect, useRef } from "react";
-import { HiOutlineLogout, HiMenu, HiX } from "react-icons/hi";
-import { VscDashboard } from "react-icons/vsc";
-import { IoPerson } from "react-icons/io5";
-import { FaUserCircle, FaHome } from "react-icons/fa";
-import { FaUsersGear } from "react-icons/fa6";
-import { BiCommentDetail } from "react-icons/bi";
-import { MdEventNote, MdOutlineArticle } from "react-icons/md";
-import { GiPublicSpeaker } from "react-icons/gi";
-import { IoMdNotificationsOutline } from "react-icons/io";
-// import { AiFillSolution } from "react-icons/ai";
-import { FaTasks } from "react-icons/fa";
-
-import { useSelector, useDispatch } from "react-redux";
-import { Link, useNavigate, useLocation } from "react-router-dom";
-import { logoutUser } from "../../features/auth/authSlice";
-import { fetchProfileById } from "../../features/Users/userAction";
-
+import React, { useState } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import { Snackbar } from "@mui/material";
+import MuiAlert from "@mui/material/Alert";
+import Spinner from "../components/tools/Spinner";
 const backendURL = import.meta.env.VITE_BACKEND_URL;
 
-const AdminSidebar = () => {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const location = useLocation();
-  const [isOpen, setIsOpen] = useState(false);
-  const [isSmallScreen, setIsSmallScreen] = useState(false);
-  const user = useSelector((state) => state.auth.user);
-  const sidebarRef = useRef(null);
-  const { userInfo } = useSelector((state) => state.auth);
-  const userId = userInfo?.user._id;
-  const { profile, loading, success, error } = useSelector(
-    (state) => state.profiles
-  );
+const Alert = React.forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
 
-  useEffect(() => {
-    if (userId) {
-      dispatch(fetchProfileById(userId));
+const ResetPassword = () => {
+  const { token } = useParams(); // Get the reset token from the URL
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [openSnackbar, setOpenSnackbar] = useState(false);
+  const [snackbarSeverity, setSnackbarSeverity] = useState("success");
+
+  const handleCloseSnackbar = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
     }
-  }, [dispatch, userId]);
-
-  const toggleSidebar = () => setIsOpen(!isOpen);
-
-  const handleLogout = () => {
-    dispatch(logoutUser());
-    navigate("/");
+    setOpenSnackbar(false);
   };
 
-  useEffect(() => {
-    const handleResize = () => {
-      setIsSmallScreen(window.innerWidth <= 768);
-      if (window.innerWidth > 768) {
-        setIsOpen(true);
-      } else {
-        setIsOpen(false);
-      }
-    };
-
-    window.addEventListener("resize", handleResize);
-    handleResize();
-
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
-  const sidebarLinks = [
-    { path: "/", icon: FaHome, label: "Home" },
-    {
-      path: "/DashBoard/Admin_Dashboard",
-      icon: VscDashboard,
-      label: "Dashboard",
-    },
-    { path: "/DashBoard/AdminProfile", icon: IoPerson, label: "Profile" },
-    { path: "/DashBoard/Users", icon: FaUsersGear, label: "Users" },
-    {
-      path: "/DashBoard/Admin/Comments",
-      icon: BiCommentDetail,
-      label: "Comments",
-    },
-    {
-      path: "/DashBoard/Admin/Posts",
-      icon: MdOutlineArticle,
-      label: "Blog Posts",
-    },
-    { path: "/DashBoard/Admin/Events", icon: MdEventNote, label: "Events" },
-    { path: "/DashBoard/Admin/Authors", icon: IoPerson, label: "Authors" },
-    {
-      path: "/DashBoard/Admin/SpeakerList",
-      icon: GiPublicSpeaker,
-      label: "Speakers",
-    },
-    {
-      path: "/DashBoard/Admin/Solutions",
-      icon: FaTasks,
-      label: "Solutions",
-    },
-    {
-      path: "/DashBoard/Admin/RegisteredSolutionsList",
-      icon: IoMdNotificationsOutline,
-      label: "Registered Solutions",
-    },
-  ];
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const response = await axios.post(
+        `${backendURL}/api/reset-password/${token}`,
+        {
+          password,
+        }
+      );
+      setMessage(response.data.message);
+      setSnackbarSeverity("success");
+      setPassword("");
+    } catch (error) {
+      setMessage(error.response?.data?.message || "An error occurred");
+      setSnackbarSeverity("error");
+    } finally {
+      setLoading(false);
+      setOpenSnackbar(true);
+      setTimeout(() => {
+        navigate("/login");
+      }, 3000);
+    }
+  };
 
   return (
     <>
-      <div
-        className={`fixed top-0 left-0 right-0 z-40 bg-white shadow-md p-4 flex items-center justify-between ${
-          isSmallScreen ? "" : "hidden"
-        }`}
-      >
-        <div className="flex items-center">
-          <span className="mb-2">
-            {profile?.image ? (
-              <img
-                src={`${backendURL}/uploads/${profile?.image}`}
-                alt={`${userInfo.username}`}
-                className="w-7 h-7 rounded-full object-cover mr-4"
-                onError={(e) => {
-                  e.target.onerror = null;
-                  e.target.src = "/fallback-image.png";
-                }}
-              />
-            ) : (
-              <FaUserCircle className="w-7 h-7 text-gray-400 mr-4 cursor-pointer" />
-            )}
-          </span>
-          <span className="mb-1 mr-2 font-medium text-xs">
-            {profile?.username || "Guest"}
-          </span>
-        </div>
-        <button
-          onClick={toggleSidebar}
-          className="text-purple-600 p-2 rounded-md focus:outline-none"
-        >
-          {isOpen ? <HiX size={24} /> : <HiMenu size={24} />}
-        </button>
-      </div>
-
-      <div
-        ref={sidebarRef}
-        className={`fixed inset-y-0 left-0 z-30 w-64 bg-purple-700 text-white transition-all duration-300 ease-in-out transform
-          ${isOpen ? "translate-x-0" : "-translate-x-full"}
-          ${isSmallScreen ? "top-16" : "top-0"}`}
-      >
-        <div className="flex flex-col h-full">
-          <div className="p-5">
-            <h1 className="text-2xl font-bold mb-1">Credulen</h1>
-            <p className="text-sm text-purple-300 mb-6">Administrator</p>
-            {!isSmallScreen && (
-              <div className="flex items-center">
-                <span className="mb-2">
-                  {profile?.image ? (
-                    <img
-                      src={`${backendURL}/uploads/${profile?.image}`}
-                      alt={`${userInfo.username}`}
-                      className="w-7 h-7 rounded-full object-cover mr-4"
-                      onError={(e) => {
-                        e.target.onerror = null;
-                        e.target.src = "/fallback-image.png";
-                      }}
-                    />
-                  ) : (
-                    <FaUserCircle className="w-7 h-7 text-gray-400 mr-4 cursor-pointer" />
-                  )}
-                </span>
-                <span className="mb-1 mr-2 font-extralight text-xs">
-                  {profile?.username || "Guest"}
-                </span>
-              </div>
-            )}
+      <div className="md:mt-32 mt-36 md:px-[5rem] p-16 border-2 mx-auto md:w-[35rem] mid:mx-[1rem] rounded-xl">
+        <h2 className="mb-10 flex justify-center text-xl">Reset Password</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-5">
+            <label
+              htmlFor="password"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              New Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Enter your new password"
+              required
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            />
           </div>
 
-          <nav className="flex-grow">
-            {sidebarLinks.map((link) => (
-              <Link
-                key={link.path}
-                to={link.path}
-                onClick={() => isSmallScreen && setIsOpen(false)}
-                className={`flex items-center px-5 py-3 text-sm ${
-                  location.pathname === link.path
-                    ? "bg-purple-800"
-                    : "hover:bg-purple-600"
-                }`}
-              >
-                <link.icon className="w-5 h-5 mr-3" />
-                <span>{link.label}</span>
-              </Link>
-            ))}
-          </nav>
-
-          <div className="p-5">
+          <div className="flex justify-end">
             <button
-              onClick={handleLogout}
-              className="w-full flex items-center px-5 py-3 text-sm bg-red-500 text-white rounded-md hover:bg-red-600 transition-colors"
+              type="submit"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mt-5 mb-5"
             >
-              <HiOutlineLogout className="w-5 h-5 mr-3" />
-              <span>Sign out</span>
+              {loading ? <Spinner /> : "Set New Password"}
             </button>
           </div>
-        </div>
+        </form>
       </div>
 
-      <div
-        className={`transition-all duration-300 ${
-          isOpen && !isSmallScreen ? "ml-64" : "ml-0"
-        }`}
-      ></div>
+      {/* Snackbar for notifications */}
+      <Snackbar
+        open={openSnackbar}
+        autoHideDuration={6000}
+        onClose={handleCloseSnackbar}
+      >
+        <Alert
+          onClose={handleCloseSnackbar}
+          severity={snackbarSeverity}
+          sx={{ width: "100%" }}
+        >
+          {message}
+        </Alert>
+      </Snackbar>
     </>
   );
 };
 
-export default AdminSidebar;
+export default ResetPassword;
